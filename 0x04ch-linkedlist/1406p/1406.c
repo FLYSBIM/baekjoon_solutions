bfd0321fc6b694890a9d5e9d98175180f1a17543
seongb._.bb <jj2358971@gmail.com>
2023-10-11 20:01:53 +0900

    1406p solution

--- 0x04ch-linkedlist/1406p/1406.c
+++ 0x04ch-linkedlist/1406p/1406.c
@@ -1,76 +1,149 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+//구조체 정의V
 typedef struct Node{
 	char ch;
 	struct Node*next;
 	struct Node*back;
 }Node;
 
-void del(Node *node){
-	if(node->back){
-		node->back->next=node->next;
+//실제문자열 길이V
+int strlength(char*str){
+	int strlength=0;
+	while(str[strlength]!='\0'){
+		strlength++;
 	}
-	if(node->next){
-		node->next->back=node->back;
-	}
-	node->next=NULL;
-	node->back=NULL;
+	return strlength;
 }
-int main(void){
-	char str[100001]={0};
-	int i=0;
-	int n=0;
-	scanf("%s",str);
-	while(str[i]!='\0'){
-		i++;
-	}
-
-	Node narr[i];
-	Node*nownode=NULL;
-	for (int j = 0; j < i; j++) {
-        	narr[j].ch = str[j];
-        	if (j < i - 1) {
-        	    narr[j].next = &narr[j + 1];
-            	    narr[j + 1].back = &narr[j];
-        	} 
-		else {
-            		narr[j].next = NULL;
-        	}
-    	}		
-	narr[0].back=NULL;
-	nownode=&narr[i-1]; 
-
-	scanf("%d",&n);
-	char com='\0';
-	for(int k=0;k<n;k++){
-		scanf(" %c",&com);
-		
-		switch(com){
-			case 'P':		
-				
-			case 'B':
-				if (nownode&&nownode->back) {
-        				Node* toDel = nownode;
-        				nownode = nownode->back;
-        				del(toDel);
-    				}
-				break;
-
-			case 'L':
-				nownode=nownode->back;
-				break;
-			case 'D':
-				nownode=nownode->next;
-				break;
+//연결리스트에 들어있는 문자 출력
+void printNode(Node *pNode){
+	while(1){
+		if(pNode->back==NULL){
+			break;
 		}
+		pNode=pNode->back;
+	}
+	
+	while(1){
+		if(pNode->next==NULL){
+			printf("%c",pNode->ch);
+			break;
+		}
+
+		if(pNode->ch!='\0'){
+			printf("%c",pNode->ch);
+		}
+		pNode=pNode->next;
+	}
+	putchar('\n');
+}
+
+Node*delNode(Node*pNode){
+	Node*backNode=pNode->back;
+	if(pNode->ch=='\0'){
+		return pNode;
+	}	
+	
+
+	if(pNode->back!=NULL){
+		pNode->back->next=pNode->next;
+	}
+	
+	if(pNode->next!=NULL){
+		pNode->next->back=pNode->back;
 	}
 
-	for(int j=0;j<i;j++){
-		printf("%c",narr[j].ch);
+	free(pNode);
+	pNode=NULL;
+
+	return backNode;
+}
+
+void freeNode(Node*tNode){
+	while(tNode->back!=NULL){
+		tNode=tNode->back;
+	}
+	while(tNode!=NULL){
+		Node*temp=tNode;
+		tNode=tNode->next;
+		free(temp);
+	}
+}
+
+Node* getNode(char ch){
+	Node*get=(Node*)malloc(sizeof(Node));
+	get->ch=ch;
+	get->next=NULL;
+	get->back=NULL;
+	return get;
+}
+
+Node* addNode(Node*pNode,char ch){
+	Node*ad=getNode(ch);
+
+	if(pNode->ch=='\0'){
+		pNode->ch=ch;
+		pNode->back=getNode('\0');
+		pNode->back->next=pNode;
+		return pNode;
 	}
 
+	ad->next=pNode->next;
+	ad->back=pNode;	
+	if(pNode->next){
+		pNode->next->back=ad;
+	}
+	pNode->next=ad;
+	return ad;
+}
 
-	printf("\n");
+int main(void){
+
+	char str[600001]="0";
+	scanf("%s",str);
+
+	int commandN=0;
+	scanf("%d",&commandN);
+	
+	int strleng=strlength(str);
+
+	Node*start=getNode('\0');	
+	Node*pNode=start;
+
+	for(int i=0;i<strleng;i++){
+		Node*tnode=getNode(str[i]);
+	
+		tnode->back=pNode;
+		pNode->next=tnode;
+		pNode=pNode->next;
+	}
 		
-		
+
+	for(int i=0;i<commandN;i++){
+		char command;
+		scanf(" %c",&command);
+		if(command=='L'){
+			if(pNode->back!=NULL){
+				pNode=pNode->back;
+			}
+		}
+		else if(command=='D'){
+			if(pNode->next!=NULL){
+				pNode=pNode->next;
+			}
+		}
+		else if(command=='B'){
+			if(pNode!=NULL){
+				pNode=delNode(pNode);
+			}
+		}
+		else if(command=='P'){
+			char addchar='\0';
+			scanf(" %c",&addchar);
+			pNode=addNode(pNode,addchar);
+		}	
+	}
+	printNode(pNode);
+	freeNode(pNode);
 }
